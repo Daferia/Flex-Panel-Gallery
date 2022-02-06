a2f3d7c028dcf65cf060a0bdf86e46600a52ec5e
Fabio Faria <daferia@gmail.com>
2022-02-06 11:39:40 +0000

    added comments to js

--- script.js
+++ script.js
@@ -2,15 +2,18 @@ const panels = document.querySelectorAll('.panel');
 
 function toggleOpen(){
     console.log('Hello');
-    this.classList.toggle('open')
+    this.classList.toggle('open') // adds the clss open when toggle to panel
 };
 
 function toggleActive(e){
     console.log(e.propertyName);
-    if(e.propertyName.includes('flex')){
-        this.classList.toggle('open-active');
+    if(e.propertyName.includes('flex')){ // looking for word flex in CSS propert due to browser naming conventions on flex/flex-box
+        this.classList.toggle('open-active'); // adds the .open-active class when toggled
     }
 }
 
+//loops through each panel and looks for click event and runs function
 panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+
+//loops through each panel and looks for transitionend event and runs function
 panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
